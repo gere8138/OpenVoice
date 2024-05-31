2371a9ddcb3648c68de6ea40b500b9852f26a765
Pongpanod Worarittichai <128355952+gere8138@users.noreply.github.com>
2024-05-31 13:28:30 +0700

    Update run3.py

--- run3.py
+++ run3.py
@@ -13,17 +13,16 @@ tone_color_converter.load_ckpt(f'{ckpt_converter}/checkpoint.pth')
 
 os.makedirs(output_dir, exist_ok=True)
 
-reference_speaker = 'resources/ahri_1m.mp3' # This is the voice you want to clone
-target_se, audio_name = se_extractor.get_se(reference_speaker, tone_color_converter, None, vad=False)
+reference_speaker = 'voices/' # This is the voice you want to clone, make folder called voices then write voices/[file name] here
+target_se, audio_name = se_extractor.get_se(reference_speaker, tone_color_converter, None, vad=False) #instead of none, write folder file in processed to skip training period
 
 from melo.api import TTS
-
 
 inputs = [
     [1,'EN_NEWEST', "Bread is a staple food prepared from a dough of flour usually wheat and water, usually by baking. Throughout recorded history and around the world, it has been an important part of many cultures' diet. It is one of the oldest human-made foods, having been of significance since the dawn of agriculture, and plays an essential role in both religious rituals and secular culture."],
     [2,'JP', "夢ならばどれほどよかったでしょう。未だにあなたのことを夢にみる。忘れた物を取りに帰るように。古びた思い出の埃を払う。戻らない幸せがあることを。最後にあなたが教えてくれた。言えずに隠してた昏い過去も。あなたがいなきゃ永遠に昏いまま。"],
-    #[2,'EN_NEWEST', "This portion of the audio is English."],
-    #[3, 'JP', "沈むように溶けてゆくように。二人だけの空が広がる夜に。さよならだけだった。その一言で全てが分かった。日が沈み出した空と君の姿。フェンス越しに重なっていた。"]
+    #[3,'EN_NEWEST', "This portion of the audio is English."],
+    #[4, 'JP', "沈むように溶けてゆくように。二人だけの空が広がる夜に。さよならだけだった。その一言で全てが分かった。日が沈み出した空と君の姿。フェンス越しに重なっていた。"]
 ]
 
 src_path = f'{output_dir}/tmp.wav'
